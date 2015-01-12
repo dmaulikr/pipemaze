dd5330eeb0ac9f0d4dd083115663d693770ec912
jackarendt <jack.arendt93@gmail.com>
2015-01-12 13:36:27 -0600

    Initial Commit

---
+++ Pipe MazeTests/Pipe_MazeTests.m
@@ -0,0 +1,40 @@
+//
+//  Pipe_MazeTests.m
+//  Pipe MazeTests
+//
+//  Created by Jack Arendt on 1/12/15.
+//  Copyright (c) 2015 John Arendt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Pipe_MazeTests : XCTestCase
+
+@end
+
+@implementation Pipe_MazeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
